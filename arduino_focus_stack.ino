0b8fbde65c75a29d73364b7cd1d60e946854b73f
John Boyer <john@banstead.org.uk>
2017-06-02 18:45:38 +0100

    Added camera trigger on pin PD2

--- arduino_focus_stack.ino
+++ arduino_focus_stack.ino
@@ -11,46 +11,36 @@ int cmd = 0;
 int d;
 int n;
 int moving;
+int pinState = LOW;
+int trigger_period = 500; // hold camera trigger in ms
+int anti_shake_period =1000; // delay to allow things to stop vibrating
 
+const int camera_trigger = PD2;
 
 void setup() {
   // put your setup code here, to run once:
   stepper.setup();
   Serial.begin(115200);
-  pinMode(PB6, OUTPUT);
+  pinMode(camera_trigger, OUTPUT);
+  digitalWrite(camera_trigger,HIGH); // not triggered
   
 }
 
 void loop() {
   char cmd;
-    
+
+
   // read command
   while (Serial.available() > 0) {
     cmd=Serial.parseInt(); // 1 is just a move 2 is take a photo too
     D=Serial.parseInt(); // 1 for CW and -1 for CCW
     d=Serial.parseInt(); // distance in uM
-    n=Serial.parseInt(); // number of steps
+    n=Serial.parseInt(); // number of repetitons
   }
 
   d=d*0.4; // convert um to steps
 
-  if(cmd==1) {
-    digitalWrite(11,HIGH);
-    delay(200);
-    Serial.println("High");
-    while(1){
-      delay(1);
-      }
-    }
-    if(cmd==0){
-      
-      digitalWrite(11,LOW);
-      delay(200);
-      Serial.println("Low");
-     }
-     cmd=-1;
-  
- /*
+
   if(cmd==1)                      // move
   {
     cmd=0; // avoid re-entering loop
@@ -94,6 +84,12 @@ void loop() {
           delay(1);
         }
         stepper.hardStop(SOFT); // disables motor too
+
+        // take photo
+        delay(anti_shake_period); // wait for vobration to settle
+        digitalWrite(camera_trigger,LOW); // Camera triggered
+        delay(trigger_period);
+        digitalWrite(camera_trigger,HIGH); // Camera NOT triggered
       }
 
     } 
@@ -108,10 +104,18 @@ void loop() {
           delay(1);
         }
         stepper.hardStop(SOFT); // disables motor too
+
+        // take photo
+        delay(anti_shake_period); // wait for vobration to settle
+        digitalWrite(camera_trigger,LOW); // Camera triggered
+        delay(trigger_period);
+        digitalWrite(camera_trigger,HIGH); // Camera NOT triggered
       }
     }
-  }
+   }
+}
 
- */
- }
+
+
+ 
 
